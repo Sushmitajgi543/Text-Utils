8a2583b68ce5c56d85da622ec8a24f8fc401149f
Sushmita Kumari <sushmitajgi543@gmail.com>
2021-08-15 00:44:42 +0530

    Update settings.py

--- textutility/settings.py
+++ textutility/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0qv!=txs2^**qnd(7xbozb**!#v_)jgsda0$_pt4=(d9$q9mf-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
